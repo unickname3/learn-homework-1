6195461634910fc415705c1a7c239f3b66bf5c30
Igor Dikarev <igor.dikarev@gmail.com>
2023-05-16 14:00:31 +0300

    Solve 2_if2

--- 2_if2.py
+++ 2_if2.py
@@ -15,12 +15,26 @@
 
 """
 
+def strings_compare(s1, s2):
+  if type(s1) != str or type(s2) != str:
+    return 0
+  if s1 == s2:
+    return 1
+  if len(s1) > len(s2):
+    return 2
+  if s2 == 'learn':
+    return 3
+
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    print('strings_compare(2, "learn") -> ', strings_compare(2, "learn"))
+    print('strings_compare("word", "word") -> ', strings_compare("word", "word"))
+    print('strings_compare("words", "word") -> ', strings_compare("words", "word"))
+    print('strings_compare("word", "learn") -> ', strings_compare("word", "learn"))
+    print('strings_compare("word", "words") -> ', strings_compare("word", "words"))
     
 if __name__ == "__main__":
     main()
