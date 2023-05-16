128cbfca9fc7483f6e9e462f0f7fd02d115e6b48
Igor Dikarev <igor.dikarev@gmail.com>
2023-05-16 13:18:20 +0300

    Solve 1_if1

--- 1_if1.py
+++ 1_if1.py
@@ -14,12 +14,26 @@
 
 """
 
+def activity_by_age(age):
+  if age <= 0:
+    return "Некорректное значение возраста."
+  elif 0 < age < 7:
+    return "Вы посещаете детский сад."
+  elif 7 <= age <= 17:
+    return "Вы учитесь в школе."
+  elif 17 < age <= 22:
+    return "Вы учитесь в вузе."
+  else:
+    return "Вы работаете"
+
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    age = int(input("Введите свой возраст: "))
+    activity = activity_by_age(age)
+    print(activity)
 
 if __name__ == "__main__":
     main()
