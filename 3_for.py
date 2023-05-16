4eca570f72521d58ca4cd61b108431b5eeebcc20
Igor Dikarev <igor.dikarev@gmail.com>
2023-05-16 15:11:15 +0300

    Solve 3_for

--- 3_for.py
+++ 3_for.py
@@ -16,12 +16,44 @@
 * Посчитать и вывести среднее количество продаж всех товаров
 """
 
+
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-    
+    data = [
+        {
+            "product": "iPhone 12",
+            "items_sold": [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186],
+        },
+        {
+            "product": "Xiaomi Mi11",
+            "items_sold": [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316],
+        },
+        {
+            "product": "Samsung Galaxy 21",
+            "items_sold": [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247],
+        },
+    ]
+
+    total_sold = 0
+
+    for phone in data:
+        phone_name = phone["product"]
+        total_phone_sold = sum(phone["items_sold"])
+
+        avg_phone_sold = total_phone_sold / len(phone["items_sold"])
+        total_sold += total_phone_sold
+
+        print(f"{phone_name} всего продано {total_sold} шт.")
+        print(f"Среднее количество продаж {phone_name}: {avg_phone_sold}")
+
+    print(f"Суммарное количество продаж всех товаров: {total_sold}")
+
+    avg_sold_per_phone = total_sold / len(data)
+    print(f"Среднее количество продаж всех товаров: {avg_sold_per_phone}")
+
+
 if __name__ == "__main__":
     main()
